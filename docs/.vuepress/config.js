9c750773a5fb78818b784fbc6f83e9bbddadde3f
heuuLZP <heuuLZP@163.com>
2020-07-03 20:18:51 +0800

    正则表达式第一章笔记

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,19 @@
 module.exports = {
-  base: '/arithmetic/',
-  title: '算法学习记录',
-  description: '记录算法学习过程',
+  base: '/blog/',
+  title: '前端知识体系',
+  description: '将零碎知识归纳、整合成完成的框架体系',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }]
-  ]
+  ],
+  themeConfig: {
+    nav: [
+      { text: '正则表达式', link: '/RegExp/' },
+    ],
+    sidebar: {
+      '/RegExp/': [
+        ['', '学习笔记'], /* /RegExp/README.html */
+        ['first', '第一章：字符匹配'], /* /RegExp/first.html */
+      ]
+    }
+  }
 }
---
+++ docs/RegExp/README.md
@@ -0,0 +1,5 @@
+# 正则表达式
+
+资源：[JavaScript正则表达式迷你书](https://zhuanlan.zhihu.com/p/29707385)
+
+用于记录学习过程中的疑惑、思考。
